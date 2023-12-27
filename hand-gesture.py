deebd5c388379eb8e626a4b39f1553b78b483783
Deepanker5 <kanjarladeepanker@gmail.com>
2023-12-27 17:35:02 +0200

    Getting webcam to work

--- hand-gesture.py
+++ hand-gesture.py
@@ -1,2 +1,19 @@
-print("hello world")
-print("test print")
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import cv2
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+#get webcam to work
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret , frame = cap.read()
+    cv2.imshow("mediapipe feed", frame)
+    if cv2.waitKey(10) & 0xFF ==ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Introduction 
 ## Project Brief
 This is my final project for the Digital Systems and Design degree. More information about this project will posted shortly.
+
+## Progress
+Day1: Installed all dependancies and started to get familiar with mediapipe and openCV
